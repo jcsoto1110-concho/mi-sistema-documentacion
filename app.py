221be4f479c956e07d2e250165ed15bb53ad9c76
jcsoto1110-concho <jcsoto1110@gmail.com>
2025-10-17 15:36:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1594,8 +1594,8 @@ if st.session_state.db_connected and st.session_state.db_connection is not None:
             st.markdown("#### 📁 Configuración de Carpetas")
             ruta_base_local = st.text_input(
                 "**Ruta de carpeta de archivos** *",
-                value="C:\temp\subir_archivos",
-                placeholder="C:\temp\subir_archivos",
+                value="C:\\temp\\subir_archivos",
+                placeholder="C:\\temp\\subir_archivos",
                 help="Ruta donde están todos los archivos (se buscará recursivamente)",
                 key="ruta_base_local_tab7"
             )
@@ -1753,6 +1753,7 @@ st.markdown("""
     <p>© 2024 Marathon Sports. Todos los derechos reservados.</p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
